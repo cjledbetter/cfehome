5c339e6d2e10f70668653d3ca108ee7291527902
Chris Ledbetter <chris@mrtechfixit.com>
2017-07-12 16:14:59 -0400

    update db models.py

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -6,3 +6,6 @@ class RestaurantLocation(models.Model):
     name         = models.CharField(max_length=120)
     location     = models.CharField(max_length=120, null=True, blank=True)
     category     = models.CharField(max_length=120, null=True, blank=True)
+    timestamp    = models.DateTimeField(auto_now_add=True)
+    updated    = models.DateTimeField(auto_now=True)
+#    my_date_field   = models.DateField(auto_now=False, auto_now_add=False)
