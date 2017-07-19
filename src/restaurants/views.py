01064821e5239b90edaa66c53375fa6d8d676361
cjledbetter <cjledbetter@gmail.com>
2017-07-19 17:22:29 -0400

    Adding DB functionality

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -4,13 +4,29 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.views.generic import TemplateView, ListView, DetailView
 
+from .forms import RestaurantCreateForm
 from .models import RestaurantLocation
 
-def restaurant_listview(request):
+def restaurant_createview(request):
+    template_name = 'restaurants/form.html'
+    queryset = RestaurantLocation.objects.all()
+    context = {}
+    return render(request, template_name, context)
+    
+def restaurant_listview(request,):
     template_name = 'restaurants/restaurants_list.html'
     queryset = RestaurantLocation.objects.all()
     context = {
         "object_list": queryset
+    }
+    return render(request, template_name, context)
+
+
+def restaurant_detailview(request, slug):
+    template_name = 'restaurants/restaurantlocation_detail.html'
+    obj = RestaurantLocation.objects.get(slug=slug)
+    context = {
+        "object": obj
     }
     return render(request, template_name, context)
 
@@ -19,16 +35,18 @@ class RestaurantListView(ListView):
         slug = self.kwargs.get("slug")
         if slug:
             queryset = RestaurantLocation.objects.filter(
-                Q(category__iexact=slug) |
-                Q(category__icontains=slug)
-            )
+                    Q(category__iexact=slug) |
+                    Q(category__icontains=slug)
+                )
         else:
             queryset = RestaurantLocation.objects.all()
         return queryset
 
+
 class RestaurantDetailView(DetailView):
-    queryset = RestaurantLocation.objects.all()
-    def get_object(self, *args, **kwargs):
-        rest_id = self.kwargs.get('rest_id')
-        obj = get_object_or_404(RestaurantLocation, id=rest_id) #pk=rest_id
-        return obj
+    queryset = RestaurantLocation.objects.all() #.filter(category__iexact='asian')
+    
+    # def get_object(self, *args, **kwargs):
+    #     rest_id = self.kwargs.get('rest_id')
+    #     obj = get_object_or_404(RestaurantLocation, id=rest_id) # pk = rest_id
+    #     return obj
